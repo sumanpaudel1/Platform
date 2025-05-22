44ef4242848200aeab8dc5a0b677dcc13590241e
suman01 <sumanpaudel2004@gmail.com>
2025-05-22 10:23:46 +0545

    small implementation

--- Backend/products/models.py
+++ Backend/products/models.py
@@ -378,3 +378,9 @@ class Review(models.Model):
 class ReviewImage(models.Model):
     review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="images")
     image  = models.ImageField(upload_to="reviews/")
+    
+    
+    
+    
+    
+
